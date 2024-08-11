f409a67befd372648cdf8e2297e95fc6fdfa9e48
Vishwaa-Arumugam <127880126+Vishwaa-Arumugam@users.noreply.github.com>
2024-08-11 14:18:42 +0530

    compledted the text typing effect and deparment redirection

--- frontend/src/Topbar.jsx
+++ frontend/src/Topbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Topbar.css'
 
-const Topbar = () => {
+const Topbar = ({deptName}) => {
   return (
     <div>
         <div className='t1'>
-            <h1 className='title'><span className='dept'>IT</span> Papers</h1>
+            <h1 className='title'><span className='dept'>{deptName}</span> Papers</h1>
         </div>
         <div className='t2'>
             <p>Welcome to the treasure trove! Behold, the <br/>exclusive repository of all* IT department papers—<br />CAT exams and the infamous end semester <br />ordeal. Your journey to academic enlightenment <br />begins here. Ready to conquer?</p>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Sempage from './sempage.jsx'
+// import Sempage from './sempage.jsx'
 import './index.css'
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Sempage />
+    <App/>
   </StrictMode>,
 )
