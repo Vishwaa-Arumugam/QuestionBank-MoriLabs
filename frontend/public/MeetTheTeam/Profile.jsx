8fb9cad551fc5755a284698c78b827e7582d83a6
HallowSiddharth <93894137+HallowSiddharth@users.noreply.github.com>
2024-08-11 16:30:56 +0530

    Changes to About Us Page
    Added Photos and Links to the About us page, slightly increased the width of the images as well

--- frontend/public/MeetTheTeam/Profile.jsx
+++ frontend/public/MeetTheTeam/Profile.jsx
@@ -1,12 +1,12 @@
 import "./team.css";
 import image from "/src/assets/Vijay Veerasekaran.jpeg";
 
-export default function RectangleProfile({name}) {
+export default function RectangleProfile({name,imageLink,githubLink,linkedinLink,instagramLink}) {
     return (
         <div className="details">
             <div className="ima" >
                 <img 
-                    src={image} 
+                    src={imageLink} 
                     alt={name} 
     
                 />
@@ -15,15 +15,21 @@ export default function RectangleProfile({name}) {
                 {name}
             </div>
             <div className="link">
-            <button className="instagram-button">
+            <a href = {instagramLink}>
+            <button className="instagram-button" >
                 <i className="fab fa-instagram"></i>
             </button>
+            </a>
+            <a href = {linkedinLink}>
             <button className="linkedin-button">
                 <i className="fab fa-linkedin"></i>
             </button>
+            </a>
+            <a href = {githubLink}>
             <button className="github-button">
                 <i className="fab fa-github"></i>
             </button>
+            </a>
         </div>
         </div>
     );
