8b9b665d8d76af487865c6b897aaea01db79f305
v1sh-afk <vishalprakaash03@gmail.com>
2024-08-11 17:42:24 +0530

    footer small change

--- frontend/src/ResultPage.jsx
+++ frontend/src/ResultPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
+
 
 const ResultPage = () => {
   const param = useParams();
